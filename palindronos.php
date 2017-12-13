1495ffcd23262ff7b198c7c09b47d5370f1dcbbe
Erick Val <erick@fval.com.br>
2017-12-13 19:28:29 -0200

    Adicionada função de verificação de parâmetros de entrada e mensagem de erro caso mensagens não sejam válidas.

--- palindronos.php
+++ palindronos.php
@@ -2,10 +2,16 @@
 
 $numInicial = readline("Digite um número: ");
 $numFinal = readline("Digite outro número: ");
-
-for ($num = $numInicial; $num <= $numFinal; $num++) {
-    $sNum = (string)$num;
-    if ($sNum === strrev($sNum)) {
-        echo " ".$num;
+if($numInicial >= 0 && $numFinal > $numInicial) {
+    for ($num = $numInicial; $num <= $numFinal; $num++) {
+        $sNum = (string)$num;
+        if ($sNum === strrev($sNum)) {
+            echo " ".$num;
+        }
     }
+}
+else {
+    echo "Verifique se os números são respectivamentes:
+        .Maior ou igual a zero
+        .Maior que o número anterior";
 }
