0bfcab29feba4dc1a2bd06621349786499a64432
Erick Val <erick@fval.com.br>
2017-12-13 18:07:07 -0200

    Exercício primos

---
+++ primos.php
@@ -0,0 +1,17 @@
+<?php
+/* Criar um programa que :      liste todos os números primos entre 1 e 1000.
+Condições:
+1. O programa deve ser executado por linha de comando.
+2. Os números deverão ser impressos lado a lado, separados por espaço
+*/
+
+
+for ($numPrimo = 1; $numPrimo < 1001; $numPrimo++)
+{
+    $num = gmp_prob_prime($numPrimo);
+    if($num == 2)
+    {
+        echo " ".$numPrimo;
+    }
+
+}
